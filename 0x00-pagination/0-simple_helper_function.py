1a9f08ee1fd0f1a15ba2c75f3e76ca6980081e7c
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-22 19:20:58 +0000

    0 simple helper function updated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,6 +9,7 @@ Page numbers are 1-indexed, i.e. the first page is page 1.
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
      return a tuple of size two containing a start index
@@ -16,9 +17,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = 0
     end_index = 0
-    
+   
     for i in range(page):
         start_index = end_index
         end_index += page_size
     return (start_index, end_index)
-    
