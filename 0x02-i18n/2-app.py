052710204dedb75391b8b1d487fdded4ba08da96
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-01 17:57:24 -0400

    New Updates

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -35,7 +35,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Handles / route
+    Handles  / route
     """
     return render_template('2-index.html')
 
